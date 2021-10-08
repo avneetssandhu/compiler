d7667baebf3b3f710e8d3d653da1c67b0ccd472e
Avin <avneetssandhu@gmail.com>
2021-10-08 15:45:55 -0700

    created _is_functions directory, and created _is_functions_basic.h within _is_functions/

---
+++ _is_functions/_is_functions_basic.h
@@ -0,0 +1,138 @@
+#ifndef _IS_FUNCTIONS_BASIC_H
+#define _IS_FUNCTIONS_BASIC_H
+
+/*
+The below functions check if the character passed to them
+is the lowercase version of the character specified after
+"_is_character_(a/an)" in the function name.
+*/
+
+int _is_character_an_A_lower(char);
+int _is_character_a_B_lower(char);
+int _is_character_a_C_lower(char);
+int _is_character_a_D_lower(char);
+int _is_character_an_E_lower(char);
+int _is_character_a_F_lower(char);
+int _is_character_a_G_lower(char);
+int _is_character_a_H_lower(char);
+int _is_character_an_I_lower(char);
+int _is_character_a_J_lower(char);
+int _is_character_a_K_lower(char);
+int _is_character_a_L_lower(char);
+int _is_character_a_M_lower(char);
+int _is_character_a_N_lower(char);
+int _is_character_an_O_lower(char);
+int _is_character_a_P_lower(char);
+int _is_character_a_Q_lower(char);
+int _is_character_a_R_lower(char);
+int _is_character_a_S_lower(char);
+int _is_character_a_T_lower(char);
+int _is_character_a_U_lower(char);
+int _is_character_an_V_lower(char);
+int _is_character_a_W_lower(char);
+int _is_character_a_X_lower(char);
+int _is_character_a_Y_lower(char);
+int _is_character_a_Z_lower(char);
+
+
+
+/*
+The below functions check if the character passed to them
+is the uppercase version of the character specified after
+"_is_character_(a/an)" in the function name.
+*/
+
+int _is_character_an_A_upper(char);
+int _is_character_a_B_upper(char);
+int _is_character_a_C_upper(char);
+int _is_character_a_D_upper(char);
+int _is_character_an_E_upper(char);
+int _is_character_a_F_upper(char);
+int _is_character_a_G_upper(char);
+int _is_character_a_H_upper(char);
+int _is_character_an_I_upper(char);
+int _is_character_a_J_upper(char);
+int _is_character_a_K_upper(char);
+int _is_character_a_L_upper(char);
+int _is_character_a_M_upper(char);
+int _is_character_a_N_upper(char);
+int _is_character_an_O_upper(char);
+int _is_character_a_P_upper(char);
+int _is_character_a_Q_upper(char);
+int _is_character_a_R_upper(char);
+int _is_character_a_S_upper(char);
+int _is_character_a_T_upper(char);
+int _is_character_a_U_upper(char);
+int _is_character_an_V_upper(char);
+int _is_character_a_W_upper(char);
+int _is_character_a_X_upper(char);
+int _is_character_a_Y_upper(char);
+int _is_character_a_Z_upper(char);
+
+
+
+/*
+The below functions check if the character passed to them
+is the character version of the number specified after
+"_is_character_a" in the function name.
+*/
+
+int _is_character_a_0(char);
+int _is_character_a_1(char);
+int _is_character_a_2(char);
+int _is_character_a_3(char);
+int _is_character_a_4(char);
+int _is_character_a_5(char);
+int _is_character_a_6(char);
+int _is_character_a_7(char);
+int _is_character_a_8(char);
+int _is_character_a_9(char);
+
+
+
+/*
+The below functions check if the character passed to them
+is the character specified after "_is_character_(a/an)" 
+in the function name.
+*/
+
+int _is_character_a_space(char);
+int _is_character_a_tab(char);
+int _is_character_a_newline(char);
+int _is_character_a_EOF(char);
+int _is_character_a_tilde(char);
+int _is_character_a_backtick(char);
+int _is_character_an_exclamation_mark(char);
+int _is_character_an_at_sign(char);
+int _is_character_a_hash_sign(char);
+int _is_character_a_dollar_sign(char);
+int _is_character_a_percent_sign(char);
+int _is_character_a_carrot_sign(char);
+int _is_character_an_ampersand(char);
+int _is_character_an_asterisk(char);
+int _is_character_an_equal_sign(char);
+int _is_character_a_plus_sign(char);
+int _is_character_a_minus_sign(char);
+int _is_character_an_underscore(char);
+int _is_character_a_l_round_bracket(char);
+int _is_character_a_r_round_bracket(char);
+int _is_character_a_l_curly_bracket(char);
+int _is_character_a_r_curly_bracket(char);
+int _is_character_a_l_angle_bracket(char);
+int _is_character_a_r_angle_bracket(char);
+int _is_character_a_l_square_bracket(char);
+int _is_character_a_r_square_bracket(char);
+int _is_character_a_pipe(char);
+int _is_character_a_backslash(char);
+int _is_character_a_colon(char);
+int _is_character_a_semicolon(char);
+int _is_character_a_double_quote(char);
+int _is_character_a_single_quote(char);
+int _is_character_a_comma(char);
+int _is_character_a_period(char);
+int _is_character_a_question_mark(char);
+int _is_character_a_forwardslash(char);
+
+
+
+#endif /* _IS_FUNCTIONS_BASIC_H */
